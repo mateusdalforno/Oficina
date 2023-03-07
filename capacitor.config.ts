c7c1ab7396f9761624dc0e25a4e974ebb4ccb116
Mateus Henrique Dal Forno <02664899074@FW.IFFAR.EDU.BR>
2023-03-07 19:34:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.oficina',
+  appName: 'oficina',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
