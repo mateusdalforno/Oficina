d79d9614b523c37f9b7b6951ce2cb2b46398f3ec
mateusdalforno <mateush.dalforno@gmail.com>
2023-03-28 19:10:10 -0300

    código aula 4

--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cliente-add-edit',
@@ -14,7 +15,59 @@ export class ClienteAddEditPage implements OnInit {
  // @ViewChild('inputNome', {read: ElementRef}) nome!: ElementRef;
   
  clienteForm!: FormGroup;
- constructor(private formBuilder: FormBuilder) { }
+
+ hasErrors = false;
+ errorMessage: string[] | undefined;
+
+ constructor(
+  private formBuilder: FormBuilder,
+  private alertController: AlertController,
+  private toastController: ToastController
+  ) {
+
+   }
+
+   async presentAlert(header: string, subHeader: string, message: string, buttons: string[]){
+    const alert = await this.alertController.create({
+      header,
+      subHeader,
+      message,
+      buttons
+    });
+    await alert.present();
+   }
+
+   async presentToast(message: string, duration:number, position: 'top' | 'bottom'){
+    const toast = await this.toastController.create({
+      message,
+      duration,
+      position
+    });
+    toast.present();
+   }
+
+ validationMessages = {
+  nome: [
+    {type: 'required', message: 'Nome é obrigatório'},
+    {type: 'minlenght', message: 'Nome deve ter ao menos 3 caracteres'},
+    {type: 'maxlenght', message: 'Nome não pode ter mais que 50 caracteres'}
+  ],
+  renda: [
+    {type: 'min', message: 'Renda precisa ser positiva'},
+    {type: 'required', message: 'Renda é obrigatória'}
+  ],
+  email: [
+    {type: 'email', message: 'Informe um e-mail válido'},
+    {type: 'required', message: 'e-mail é obrigatório'}
+  ],
+  tel: [
+    {type: 'required', message: 'Telefone é obrigatório'}
+  ],
+  nascimento: [
+    {type: 'required', message: 'Nascimento é obrigatório'}
+  ]
+ }
+
 
   ngOnInit() {
     this.clienteForm = this.formBuilder.group({
@@ -28,12 +81,30 @@ export class ClienteAddEditPage implements OnInit {
 
   public nome: string | undefined;
 
-  submit(){
-    if(this.clienteForm.invalid || this.clienteForm.pending){
-      return;
+  async submit(){
+    this.errorMessage = [];
+    if(this.clienteForm.get('nome')!.hasError('required')){
+      this.errorMessage.push('Nome é obrigatório');
+    }
+    if(this.clienteForm.get('email')!.hasError('required')){
+      this.errorMessage.push('Email é obrigatório');
+    }
+    if(this.clienteForm.get('tel')!.hasError('required')){
+      this.errorMessage.push('Telefone é obrigatório');
+    }
+    if(this.clienteForm.get('renda')!.hasError('required')){
+      this.errorMessage.push('Renda é obrigatório');
+    }
+    if(this.clienteForm.get('nascimento')!.hasError('required')){
+      this.errorMessage.push('Nascimento é obrigatório');
+    }
+    this.hasErrors = this.errorMessage.length >0;
+
+    if(!this.hasErrors){
+      await this.presentToast('Gravação bem sucedida', 3000,'top');
     }
 
-   // console.log(this.nome.nativeElement.value);
+   // console.log(this.nome.nativeElement.value);*/
   }
 
   //submit(inputNome: any, inputEmail: any, inputTelefone: any, inputRenda: any, inputNascimento: any){
